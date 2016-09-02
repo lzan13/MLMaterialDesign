3778d3c1e2837969f461bc1561cb09b4feb566ff
lzan13 <lzan13.gg@gmail.com>
2016-09-02 19:22:35 +0800

    添加测试java程序的library

---
+++ javalibrary/src/main/java/net/melove/library/java/MLMain.java
@@ -0,0 +1,15 @@
+package net.melove.library.java;
+
+/**
+ * 我们java library 的额第一个类
+ */
+public class MLMain {
+
+    /**
+     * java类的 main方法，可以直接 run
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.print("hello java library!");
+    }
+}
